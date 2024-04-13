bc09ec4076069222ae2225deaa55068da070abdf
Viswa14J <137147815+Viswa14J@users.noreply.github.com>
2024-04-13 14:02:34 +0530

    Add files via upload

--- rfid/first/urls.py
+++ rfid/first/urls.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('',views.home,name='home'),
+     path('',views.register,name='register'),
+       path('signup',views.register1,name='register1'),
+    path('home/',views.home,name='home'),
+
     path('read/',views.read,name='read'),
     path('read1/',views.read1,name='read1'),
-    path('signin/',views.signin,name='signin'),
     path('signup/',views.signup,name='signup'),
-    path('write/',views.write,name='write'),
-    path('write2/',views.write2,name='write2'),
+    path('write/',views.register_w1,name='register_w1'),
+    path('write2/',views.register_w2,name='register_w2'),
     path('track/',views.track,name='track'),
-    
+    path('track2/',views.track2,name='track2'),
 
 ]
-
-
